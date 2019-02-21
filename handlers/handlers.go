c6cab639061aa708a04e3353f01f6f80db67a77b
Sandro Wong <saloiofun@gmail.com>
2019-02-21 08:47:24 -0800

    revert

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,6 @@
 package handlers
 
 import (
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -13,36 +8,48 @@ import (
 
 // Tableflip is here.
 func Tableflip(c *gin.Context) {
-	channel := c.PostForm("channel_id")
 	text := strings.Split(c.PostForm("text"), " ")[0]
-	methodURL := "https://slack.com/api/chat.postMessage"
 	ascii := "(╯°□°）╯︵ ┻━┻"
 
 	if text == "down" {
 		ascii = "┬─┬ノ( º _ ºノ)"
 	}
 
-	v := url.Values{}
-	v.Set("token", os.Getenv("SLACK_TOKEN"))
-	v.Add("channel", channel)
-	v.Add("text", ascii)
-	v.Add("as_user", "true")
+	c.JSON(200, gin.H{
+		"response_type": "in_channel",
+		"text":          ascii,
+	})
 
-	resp, err := http.PostForm(methodURL, v)
+	// channel := c.PostForm("channel_id")
+	// text := strings.Split(c.PostForm("text"), " ")[0]
+	// methodURL := "https://slack.com/api/chat.postMessage"
+	// ascii := "(╯°□°）╯︵ ┻━┻"
 
-	if err != nil {
-		log.Println("error happened getting the response", err)
-		c.String(http.StatusBadRequest, "error happened getting the response", err)
-		return
-	}
+	// if text == "down" {
+	// 	ascii = "┬─┬ノ( º _ ºノ)"
+	// }
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	// v := url.Values{}
+	// v.Set("token", os.Getenv("SLACK_TOKEN"))
+	// v.Add("channel", channel)
+	// v.Add("text", ascii)
+	// v.Add("as_user", "true")
 
-	if err != nil {
-		log.Println("error happened reading the body", err)
-		return
-	}
+	// resp, err := http.PostForm(methodURL, v)
 
-	log.Println(string(body[:]))
+	// if err != nil {
+	// 	log.Println("error happened getting the response", err)
+	// 	c.String(http.StatusBadRequest, "error happened getting the response", err)
+	// 	return
+	// }
+
+	// defer resp.Body.Close()
+	// body, err := ioutil.ReadAll(resp.Body)
+
+	// if err != nil {
+	// 	log.Println("error happened reading the body", err)
+	// 	return
+	// }
+
+	// log.Println(string(body[:]))
 }
