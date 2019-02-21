1b4a9ef1532b9a96d23f1a6616b91784c15a66a0
Sandro Wong <saloiofun@gmail.com>
2019-02-20 21:56:30 -0800

    Post oauth access

--- handlers/authorization.go
+++ handlers/authorization.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -9,28 +12,54 @@ import (
 )
 
 var (
-	slackOauthConfig = &oauth2.Config{
-		RedirectURL:  "https://slack.com/oauth/authorize",
-		ClientID:     os.Getenv("SLACK_CLIENT_ID"),
-		ClientSecret: os.Getenv("SLACK_SECRET"),
-		Scopes:       []string{"chat:write:user"},
-	}
-
 	secret       = os.Getenv("SLACK_SIGNING_SECRET")
 	clientID     = os.Getenv("SLACK_CLIENT_ID")
 	clientSecret = os.Getenv("SLACK_SECRET")
+
+	slackOauthConfig = &oauth2.Config{
+		RedirectURL:  "https://slack.com/oauth/authorize",
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       []string{"chat:write:user"},
+	}
 )
 
 // Authorization of Slack OAuth.
+// https://www.youtube.com/watch?v=OdyXIi6DGYw
 func Authorization(c *gin.Context) {
 	url := slackOauthConfig.AuthCodeURL(secret)
 	c.Redirect(http.StatusTemporaryRedirect, url)
 }
 
 // Callback for token.
+// https://golang.org/pkg/net/url/#Values
+// https://github.com/solderjs/http-examples/blob/master/go/post-form.go
 func Callback(c *gin.Context) {
 	code := c.Query("code")
+	oauthAccessURL := "https://slack.com/api/oauth.access?"
 
-	authorizeURL := "https://slack.com/api/oauth.access?" + "client_id=" + clientID + "&" + "client_secret=" + clientSecret + "&" + "code=" + code
-	http.Get(authorizeURL)
+	v := url.Values{}
+	v.Set("client_id", clientID)
+	v.Add("client_secret", clientSecret)
+	v.Add("code", code)
+
+	resp, err := http.PostForm(oauthAccessURL, v)
+
+	if err != nil {
+		log.Println("error happened getting the response", err)
+		c.String(http.StatusBadRequest, "error happened getting the response", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println("error happened reading the body", err)
+		c.String(http.StatusBadRequest, "error happened reading the body", err)
+		return
+	}
+
+	log.Println(string(body[:]))
+	c.String(http.StatusOK, string(body[:]))
 }
