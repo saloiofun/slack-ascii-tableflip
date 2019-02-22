86b57a2f6561a9314d30a11224b1975e4f0c0a45
Sandro Wong <saloiofun@gmail.com>
2019-02-21 19:44:34 -0800

    Fix oauth

--- handlers/authorization.go
+++ handlers/authorization.go
@@ -9,25 +9,28 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/slack"
 )
 
 var (
-	secret       = os.Getenv("SLACK_SIGNING_SECRET")
+	state        = os.Getenv("SLACK_SIGNING_SECRET")
 	clientID     = os.Getenv("SLACK_CLIENT_ID")
 	clientSecret = os.Getenv("SLACK_SECRET")
 
 	slackOauthConfig = &oauth2.Config{
-		RedirectURL:  "https://slack.com/oauth/authorize",
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
+		Endpoint:     slack.Endpoint,
 		Scopes:       []string{"chat:write:user"},
+		RedirectURL:  "https://guarded-wildwood-89242.herokuapp.com/slack/callback",
 	}
 )
 
 // Authorization of Slack OAuth.
 // https://www.youtube.com/watch?v=OdyXIi6DGYw
+// https://medium.com/@pliutau/getting-started-with-oauth2-in-go-2c9fae55d187
 func Authorization(c *gin.Context) {
-	url := slackOauthConfig.AuthCodeURL(secret)
+	url := slackOauthConfig.AuthCodeURL(state)
 	c.Redirect(http.StatusTemporaryRedirect, url)
 }
 
